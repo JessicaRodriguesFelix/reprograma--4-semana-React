3902ceb6428141764ef219d17cee4d3aeefb6ac6
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2018-11-07 21:41:17 -0200

    aula 3 de redux

--- Projeto/src/index.js
+++ Projeto/src/index.js
@@ -12,19 +12,13 @@ import Login from './paginas/Login/Login'
 import NaoEncontrada from './paginas/NaoEncontrada/NaoEncontrada'
 import './index.css'
 
-function App(props) {
-  const usuario = props.usuario
-  const logaUsuario = props.logaUsuario
-  const deslogaUsuario = props.deslogaUsuario
-
+function App() {
   return (
     <div className="app">
       <Navbar />
 
       <Switch>
-        <Route path="/" exact render={() => {
-          return usuario ? <Home /> : <Redirect to="/login" />
-        }} />
+        <Route path="/" exact component={Home} />
 
         <Route path="/login" component={Login}/>
         <Route path="/conta" component={Conta}/>
@@ -36,50 +30,10 @@ function App(props) {
   )
 }
 
-// const state = {
-//   usuario: { email: 'camila@email.com' }
-// }
-function passaDadosDoEstadoParaMeuComponente(state) {
-  const props = {
-    usuario: state.usuario
-  }
-
-  return props
-}
-
-function passaFuncoesQueDisparamAcoesViaProps(dispatch) {
- const props = {
-  logaUsuario: (dados) => {
-    const acao = {
-      type: 'LOGA_USUARIO',
-      dados: dados
-    }
-
-    dispatch(acao)
-  },
-  deslogaUsuario: () => {
-    const acao = {
-      type: 'DESLOGA_USUARIO'
-    }
-
-    dispatch(acao)
-  }
- }
-
- return props
-}
-
-const conectaNaStore = connect(
-  passaDadosDoEstadoParaMeuComponente, 
-  passaFuncoesQueDisparamAcoesViaProps
-)
-
-const AppConectada = withRouter(conectaNaStore(App))
-
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <AppConectada />
+      <App />
     </BrowserRouter>
   </Provider>, 
   document.getElementById('projeto')
--- Projeto/src/paginas/Home/Home.js
+++ Projeto/src/paginas/Home/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 import './Home.css'
 
-function Home() {
+function Home(props) {
+  if (!props.usuario){
+    return <Redirect to="/login"/>
+  }
+
   return (
     <main className="home">
-
     </main>
   )
 }
 
-export default Home
+export default connect(
+  (state) => ({ usuario:state.usuario })
+)(Home)
