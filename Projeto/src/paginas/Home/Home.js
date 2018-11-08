0f5056f4836ca8c5b15a9a3294a9eccf7f88d4e0
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2018-11-07 22:23:18 -0200

    commit inicio postit

--- Projeto/src/paginas/Home/Home.js
+++ Projeto/src/paginas/Home/Home.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import carregando from './carregando.svg'
 import './Home.css'
 
-function Home(props) {
-  if (!props.usuario){
-    return <Redirect to="/login"/>
+  class Home extends Component{
+    constructor(props){
+    super(props)
+    this.state = {carregando: true}
   }
+    render(){
+      if (!this.props.usuario){
+        return <Redirect to="/login"/>
+      }
+      return (
+        <main className="home">
+          {this.state.carregando ? (
+            <img className= "home__loading" src= {carregando} alt ="Carregando"/>
+          ): (
+            <div>Aqui vou listar os postits amanhã</div>
+          )}
+        </main>
+        )
+      }
+    }
 
-  return (
-    <main className="home">
-    </main>
-  )
-}
-
-export default connect(
-  (state) => ({ usuario:state.usuario })
-)(Home)
+  export default connect(
+    (state) => ({ usuario:state.usuario })
+  )(Home)
